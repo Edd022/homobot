377c87d2567eade3e50cf9acb3c9ffffc2068e0b
Danteh <boingochavez1@gmail.com>
2024-09-16 21:59:36 +0000

    feat-Abrir web con webdriver

--- bot.py
+++ bot.py
@@ -1,8 +1,28 @@
-import pyautogui
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
 import time
+# Ruta al GeckoDriver
+driver_path = '/home/kali/Downloads/geckodriver'
 
-time.sleep(5)
+# Configura el servicio de GeckoDriver
+service = Service(executable_path=driver_path)
 
-pyautogui.moveTo(100, 200)
-pyautogui.click()
-pyautogui.write('Hola desde Python!', interval=0.1)
+# Configura las opciones de Firefox
+options = Options()
+options.set_preference("dom.webdriver.enabled", False)
+options.set_preference("useAutomationExtension", False)
+options.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+
+# Inicializa el WebDriver de Firefox usando el servicio y las opciones
+driver = webdriver.Firefox(service=service, options=options)
+
+try:
+    # Abre la página web
+    driver.get('https://www.livejasmin.com/es/girls')
+    time.sleep(50)
+
+finally:
+    # Cierra el navegador
+    driver.quit()
